b47f0fcca0081ad3d5cdbaa6d2e164d76069c499
validexpress <piotr.selak.it@gmail.com>
2022-04-21 00:41:59 +0200

    Final changes

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,12 @@
 import net.ruippeixotog.scalascraper.browser.JsoupBrowser
-
+import net.ruippeixotog.scalascraper.dsl.DSL._
+import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
+import net.ruippeixotog.scalascraper.dsl.DSL.Parse._
 
 object Main extends App {
-    println("Initialization")
+    val browser = JsoupBrowser()
+    val doc = browser.get("https://www.reddit.com/r/ProgrammerHumor/")
+    val postItems = doc >> elementList("a div h3")
+    val items = postItems.map(_ >> allText("h3"))
+    println(items) 
 }
